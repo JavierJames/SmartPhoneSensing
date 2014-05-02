ab416cb3814352eeb682d66ead0ae31f16a4d191
LuisCardoso <luis.moreiracardoso@gmail.com>
2014-05-02 20:27:49 +0200

    Name fixed

--- src/com/example/smartphonesensing2/DB.java
+++ src/com/example/smartphonesensing2/DB.java
@@ -15,17 +15,18 @@ public class DB extends SQLiteOpenHelper {
     private static final String TEXT_TYPE = " TEXT";
     private static final String COMMA_SEP = ",";
     private static final String SQL_CREATE_ENTRIES =
-    	    "CREATE TABLE " + ActivityTable.NAME + " (" +
+    	    "CREATE TABLE " + ActivityTable.TABLE_NAME + " (" +
     	    ActivityTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
-    	    ActivityTable.ID + TEXT_TYPE + COMMA_SEP +
-    	    ActivityTable.X + TEXT_TYPE + COMMA_SEP +
-    	    ActivityTable.Y + TEXT_TYPE + COMMA_SEP +
-    	    ActivityTable.Z + TEXT_TYPE +
+    	    ActivityTable.FIELD_ID + TEXT_TYPE + COMMA_SEP +
+    	    ActivityTable.FIELD_X + TEXT_TYPE + COMMA_SEP +
+    	    ActivityTable.FIELD_Y + TEXT_TYPE + COMMA_SEP +
+    	    ActivityTable.FIELD_Z + TEXT_TYPE + COMMA_SEP +
+    	    ActivityTable.FIELD_ACTIVITY + TEXT_TYPE +
     	    " );";
     
     // query statement to delete entries
     private static final String SQL_DELETE_ENTRIES =
-    	    "DROP TABLE IF EXISTS " + ActivityTable.NAME;
+    	    "DROP TABLE IF EXISTS " + ActivityTable.TABLE_NAME;
 
 
     
@@ -54,11 +55,11 @@ public class DB extends SQLiteOpenHelper {
     
     /* Inner class that defines the table activity */
     public static abstract class ActivityTable implements BaseColumns {
-        public static final String NAME = "activity";
-        public static final String ID = "id";
-        public static final String X = "x";
-        public static final String Y = "y";
-        public static final String Z = "z";
-        public static final String ACTIVITY = "activity";
+        public static final String TABLE_NAME = "activity";
+        public static final String FIELD_ID = "id";
+        public static final String FIELD_X = "x";
+        public static final String FIELD_Y = "y";
+        public static final String FIELD_Z = "z";
+        public static final String FIELD_ACTIVITY = "activity";
     }
 }
