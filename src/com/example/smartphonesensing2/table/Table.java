f1988158e0f5c961ade32e4455fefb1fc02a490e
LuisCardoso <luis.moreiracardoso@gmail.com>
2014-06-15 19:26:10 +0200

    implementing listview in tab select AP

--- src/com/example/smartphonesensing2/table/Table.java
+++ src/com/example/smartphonesensing2/table/Table.java
@@ -3,10 +3,11 @@ package com.example.smartphonesensing2.table;
 import android.util.Log;
 
 public class Table {
+	//name is equal to access point
 	private String name;
 	
 	// pmf table: row (cell) ranges from 1 to 17 and column (rssi) ranges from 0 to 100
-	private float [][] pmf = new float[17][100];
+	private Float [][] table = new Float[17][101];
 	
 	
 	/*
@@ -14,6 +15,15 @@ public class Table {
 	 */
 	public Table(String name) {
 		this.name = name;
+		
+		//initialize table. necessary to prevent error when doing  floatValue, and a null is present
+	/*	for(int i=0; i <table.length; i++)
+		{
+			for(int t=0; t<table[0].length; t++)
+				table[i][t]=0f;
+		}
+	*/
+		
 	}
 	
 	
@@ -28,29 +38,54 @@ public class Table {
 	/*
 	 * Set the pmf of this table at a given row (cell) and column (rssi)
 	 */
-	public void setPMF(int cell, int rssi, float pmf) {
-		this.pmf[cell][rssi] = pmf;
+	public void setValue(int cell, int rssi, float pmf) {
+		this.table[cell][rssi] = new Float(pmf);
 	}
 	
 	
 	/*
 	 * Returns the pmf of this table at a given row (cell) and column (rssi)
+	 * if not using initializing of the 2D array at the constructor, return 0 for values which are null
+	 * uncomment the method which is desired, and comment the other
 	 */
-	public float getPMF(int cell, int rssi) {
-		return this.pmf[cell][rssi];
+	public float getValue(int cell, int rssi) {
+		if(table[cell][rssi] ==null)
+		{
+			return 0;
+		}
+		else 
+		{
+			return this.table[cell][rssi].floatValue();
+		}
+		
+	
 	}
 	
+	public Float getValue2(int cell, int rssi) {
+		return this.table[cell][rssi];
+	
+	}
+	
+	
+	/*
+	 * Returns the table
+	 */
+	public Float[][] getTable() {
+		return table;
+	}
+		
 	
 	/*
 	 * This method prints the table
 	 */
 	public void printTable() {
-		Log.d("TableName", name);
-		
-		for(int i = 0; i < pmf.length; i++) {
-			for(int j = 0; j < pmf[i].length; j++) {
-				if(pmf[i][j] > 0) {
-					Log.d("TableValue", ""+"i:" +i+1+ " j:"+ j +" = "+ pmf[i][j]);
+	//	Log.d("TableName", name);
+		System.out.println("TableName: "+ name);
+		for(int i = 0; i < table.length; i++) {
+			for(int j = 0; j < table[i].length; j++) {
+				if((table[i][j] != null) && ((table[i][j]).floatValue() > 0)) {
+	//				Log.d("TableValue", ""+"i:" +i+1+ " j:"+ j +" = "+ pmf[i][j]);
+					System.out.println("TableValue [i:"+ (i+1)+ "]  " + "[j:"+ j +"] = "+ table[i][j]);
 				}
 			}
 		}
